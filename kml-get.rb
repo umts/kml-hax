41daa2319986fee6e3f1e2c7f272c8bac0329f56
Matt Moretti <werebus@gmail.com>
2018-08-31 14:55:58 -0400

    Bundler, avail library.

--- kml-get.rb
+++ kml-get.rb
@@ -1,45 +1,5 @@
 #!/usr/bin/env ruby
-
-require 'json'
-require 'net/http'
-require 'nokogiri'
-require 'pry'
-
-module Avail
-  def self.url(*path_components)
-    uri_array = ['https://bustracker.pvta.com/InfoPoint'] + path_components
-    URI(uri_array.join('/'))
-  end
-
-  def self.json_get(uri)
-    JSON.parse(Net::HTTP.get uri)
-  end
-
-  def self.kml_get(uri)
-    Nokogiri::XML(Net::HTTP.get uri)
-  end
-
-  def self.routes
-    json_get(url %w[rest Routes GetAllRoutes])
-  end
-
-  def self.kml_for(route)
-    kml_get(url 'Resources', 'Traces', route['RouteTraceFilename'])
-  end
-
-  def self.kml_color(doc)
-    doc.css('#routestyle color').text
-  end
-
-  def self.color_difference(html, kml)
-    hrg, hb = html.hex.divmod 256
-    hr, hg = hrg.divmod 256
-    kabg, kr = kml.hex.divmod 256
-    kab, kg = kabg.divmod 256
-    kb = kab % 256
-    Math.sqrt((hr - kr)**2 + (hg - kg)**2 + (hb - kb)**2)
-  end
-end
+require_relative 'avail.rb'
 
 Avail.routes.each do |route|
   filename = route['RouteTraceFilename']
