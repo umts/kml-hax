6ad6857cbd3dc104c5427682e38c466064ecf5b4
Matt Moretti <werebus@gmail.com>
2018-09-04 13:33:47 -0400

    Added route id for nameless routes.

--- kml-check.rb
+++ kml-check.rb
@@ -8,7 +8,7 @@ Avail.routes.each do |route|
   #puts route['RouteAbbreviation'], route_color, Avail.kml_color(Avail.kml_for(route))
   r, g, b = route_color[0,2], route_color[2,2], route_color[4,2]
   unless kml_color =~ /[0-9A-F]{2}#{b+g+r}/
-    puts ["Route: #{route['RouteAbbreviation']}",
+    puts ["Route: #{route['RouteAbbreviation']} (#{route['RouteId']})",
           "Color: #{route_color}",
           "KML Color: #{kml_color}",
           "Difference: #{Avail.color_difference(route_color, kml_color)}"].join(', ')
